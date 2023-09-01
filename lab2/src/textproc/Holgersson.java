8d61fe92dd8212a028d0954481bd7a9c4f4932ee
Tvånar <eandr98@gmail.com>
2023-09-01 21:05:06 +0200

    in progress

--- lab2/src/textproc/Holgersson.java
+++ lab2/src/textproc/Holgersson.java
@@ -2,6 +2,7 @@ package textproc;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Holgersson {
@@ -12,21 +13,31 @@ public class Holgersson {
 			"öland", "östergötland" };
 
 	public static void main(String[] args) throws FileNotFoundException {
-		
-		TextProcessor p = new SingleWordCounter("nils");
 
-		Scanner s = new Scanner(new File("nilsholg.txt"));
+		ArrayList<TextProcessor> pList = new ArrayList<TextProcessor>();
+		pList.add(new SingleWordCounter("nils"));
+		pList.add(new SingleWordCounter("norge"));
+		pList.add(new MultiWordCounter(REGIONS));
+		
+
+
+		Scanner s = new Scanner(new File("lab2\\n" + //
+				"ilsholg.txt"));
 		s.findWithinHorizon("\uFEFF", 1);
 		s.useDelimiter("(\\s|,|\\.|:|;|!|\\?|'|\\\")+"); // se handledning
 
 		while (s.hasNext()) {
 			String word = s.next().toLowerCase();
-
-			p.process(word);
+			for(TextProcessor p: pList) {
+				p.process(word);
+			}
+			
 		}
 
 		s.close();
 
-		p.report();
+		for(TextProcessor p: pList) {
+			p.report();
+		}
 	}
 }
