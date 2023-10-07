3872579bb5bd4d9341a297154b72273651dea964
Tvånar <eandr98@gmail.com>
2023-10-07 21:54:36 +0200

    does not work, will fix

--- lab4/src/queue_delegate/FifoQueue.java
+++ lab4/src/queue_delegate/FifoQueue.java
@@ -1,4 +1,5 @@
 package queue_delegate;
+
 import java.util.*;
 
 public class FifoQueue<E> extends AbstractQueue<E> implements Queue<E> {
@@ -8,65 +9,70 @@ public class FifoQueue<E> extends AbstractQueue<E> implements Queue<E> {
 		super();
 		list = new LinkedList<E>();
 	}
-	
-	/**	
+
+	/**
 	 * Inserts the specified element into this queue, if possible
-	 * post:	The specified element is added to the rear of this queue
-	 * @param	e the element to insert
-	 * @return	true if it was possible to add the element 
-	 * 			to this queue, else false
+	 * post: The specified element is added to the rear of this queue
+	 * 
+	 * @param e the element to insert
+	 * @return true if it was possible to add the element
+	 *         to this queue, else false
 	 */
 	public boolean offer(E e) {
 		return list.add(e);
 	}
 
-	/**	
+	/**
 	 * Returns the number of elements in this queue
+	 * 
 	 * @return the number of elements in this queue
 	 */
-	public int size() {		
+	public int size() {
 		return list.size();
 	}
-	
-	/**	
-	 * Retrieves, but does not remove, the head of this queue, 
+
+	/**
+	 * Retrieves, but does not remove, the head of this queue,
 	 * returning null if this queue is empty
-	 * @return 	the head element of this queue, or null 
-	 * 			if this queue is empty
+	 * 
+	 * @return the head element of this queue, or null
+	 *         if this queue is empty
 	 */
 	public E peek() {
 		return list.peek();
 	}
 
-	/**	
-	 * Retrieves and removes the head of this queue, 
+	/**
+	 * Retrieves and removes the head of this queue,
 	 * or null if this queue is empty.
-	 * post:	the head of the queue is removed if it was not empty
-	 * @return 	the head of this queue, or null if the queue is empty 
+	 * post: the head of the queue is removed if it was not empty
+	 * 
+	 * @return the head of this queue, or null if the queue is empty
 	 */
 	public E poll() {
 		return list.poll();
 	}
 
-	/**	
+	/**
 	 * Returns an iterator over the elements in this queue
+	 * 
 	 * @return an iterator over the elements in this queue
-	 */	
+	 */
 	public Iterator<E> iterator() {
 		Iterator<E> it = new Iterator<E>() {
-			
+
+			private Queue<E> temp = list;
+
 			public boolean hasNext() {
-				if(hasNext()){
-					return true;
-				}
-				return false;
+				return !temp.isEmpty();
 			}
 
 			public E next() {
-				return next();
+				if (temp.isEmpty()) {
+					throw new NoSuchElementException();
+				}
+				return temp.poll(); 
 			}
-
-
 		};
 		return it;
 	}
