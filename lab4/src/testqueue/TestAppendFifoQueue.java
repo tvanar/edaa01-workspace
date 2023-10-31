ae41c04a2e34bfe373726f30c8c99829fb04dc5b
Tvånar <eandr98@gmail.com>
2023-10-31 22:32:52 +0100

    finished

--- lab4/src/testqueue/TestAppendFifoQueue.java
+++ lab4/src/testqueue/TestAppendFifoQueue.java
@@ -15,11 +15,12 @@ import java.util.NoSuchElementException;
 import java.util.Queue;
 import java.util.Iterator;
 
+
 public class TestAppendFifoQueue {
-    private Queue<Integer> myIntQueue1;
-    private Queue<Integer> myIntQueue2;
-    private Queue<String> myStringQueue1;
-    private Queue<String> myStringQueue2;
+    private FifoQueue<Integer> myIntQueue1;
+    private FifoQueue<Integer> myIntQueue2;
+    private FifoQueue<String> myStringQueue1;
+    private FifoQueue<String> myStringQueue2;
 
     @BeforeEach
     void setUp() {
@@ -38,8 +39,62 @@ public class TestAppendFifoQueue {
     }
 
     @Test
-    void testTwoEmptyQueue() {
-        
+    void testAppendTwoEmptyQueue() {
+        myIntQueue1.append(myIntQueue2);
+        assertTrue(myIntQueue1.isEmpty(), "Wrong result from empty of queue");
+        assertEquals(0, myIntQueue1.size(), "Wrong size of Empty queue");
     }
 
+    @Test
+    void testAppendEmptyToNonEmpty() {
+        myIntQueue1.offer(1);
+        myIntQueue1.offer(2);
+        myIntQueue1.offer(3);
+        myIntQueue1.append(myIntQueue2);
+        assertEquals(3, myIntQueue1.size(), "Wrong size of queue");
+        for (int i = 1; i <= 3; i++) {
+			int k = myIntQueue1.poll();
+			assertEquals(i, k, "poll returns incorrect element");
+		}
+		assertEquals(0, myIntQueue1.size(), "Wrong size after poll");
+		assertTrue(myIntQueue1.isEmpty(), "Queue not empty after poll");
+    }
+
+    @Test
+    void testAppendNonEmptyToEmpty() {
+        myIntQueue1.offer(1);
+        myIntQueue1.offer(2);
+        myIntQueue1.offer(3);
+        myIntQueue2.append(myIntQueue1);
+        assertEquals(3, myIntQueue1.size(), "Wrong size of queue");
+        for (int i = 1; i <= 3; i++) {
+			int k = myIntQueue1.poll();
+			assertEquals(i, k, "poll returns incorrect element");
+		}
+		assertEquals(0, myIntQueue1.size(), "Wrong size after poll");
+		assertTrue(myIntQueue1.isEmpty(), "Queue not empty after poll");
+    }
+
+    @Test
+    void testAppendNonEmpty_Poll() {
+        myIntQueue1.offer(1);
+        myIntQueue1.offer(2);
+        myIntQueue1.offer(3);
+        myIntQueue2.offer(4);
+        myIntQueue2.offer(5);
+        myIntQueue2.offer(6);
+        myIntQueue1.append(myIntQueue2);
+        assertEquals(6, myIntQueue1.size(), "Wrong size of queue");
+        for (int i = 1; i <= 6; i++) {
+			int k = myIntQueue1.poll();
+			assertEquals(i, k, "poll returns incorrect element");
+		}
+		assertEquals(0, myIntQueue1.size(), "Wrong size after poll");
+		assertTrue(myIntQueue1.isEmpty(), "Queue not empty after poll");
+    }
+
+    @Test
+    void testAppendSame() {
+        assertThrows(IllegalArgumentException.class, () -> myIntQueue1.append(myIntQueue1));
+    }
 }
