c9e94458177dd631273ece008597ed1797aded44
Tvånar <eandr98@gmail.com>
2023-10-31 23:08:51 +0100

    small fixes

--- lab4/src/testqueue/TestAppendFifoQueue.java
+++ lab4/src/testqueue/TestAppendFifoQueue.java
@@ -1,7 +1,6 @@
 package testqueue;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -11,31 +10,21 @@ import org.junit.jupiter.api.Test;
 
 import queue_singlelinkedlist.FifoQueue;
 
-import java.util.NoSuchElementException;
-import java.util.Queue;
-import java.util.Iterator;
-
 
 public class TestAppendFifoQueue {
     private FifoQueue<Integer> myIntQueue1;
     private FifoQueue<Integer> myIntQueue2;
-    private FifoQueue<String> myStringQueue1;
-    private FifoQueue<String> myStringQueue2;
 
     @BeforeEach
     void setUp() {
         myIntQueue1 = new FifoQueue<Integer>();
         myIntQueue2 = new FifoQueue<Integer>();
-        myStringQueue1 = new FifoQueue<String>();
-        myStringQueue2 = new FifoQueue<String>();
     }
 
     @AfterEach
     void tearDown() {
         myIntQueue1 = null;
         myIntQueue2 = null;
-        myStringQueue1 = null;
-        myStringQueue2 = null;
     }
 
     @Test
